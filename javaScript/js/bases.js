fe0e9296115178d9639312801048016bbff6a075
Diego López <diegolopezcalle2000@gmail.com>
2022-02-21 17:33:53 +0100

    Empezando con JaviScript

---
+++ javaScript/js/bases.js
@@ -0,0 +1,23 @@
+// alert('Alerta');
+// confirm('Confirmar o cancelar');
+// prompt('¿Como te llamas?')
+
+'use strict'; // Para que los navegadores te den errores
+
+const col = [];
+
+col.push() // Agrega como último elemento
+col.pop() // Borra el último elemento
+col.shift() // Borra el primer elemento
+
+
+const objeto = Object(); // const nombre = { nombre: 'Diego', apellido: 'López' }
+
+objeto.nombre = 'Diego';
+objeto.apellido = 'López';
+
+
+
+
+
+
